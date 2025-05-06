127a9daa9c0fbac69e54455742350201a196a8e9
Ahmed Refaat <116584646+ahmedrefaat26@users.noreply.github.com>
2025-05-06 20:46:19 +0300

    Add files via upload
    lab5

---
+++ LAB 5/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/api/hello")
+    @PreAuthorize("isAuthenticated()")
+    public String hello() {
+        return "Hello, authenticated user!";
+    }
+
+    @GetMapping("/api/secret")
+    @PreAuthorize("isAuthenticated()")
+    public String secret() {
+        return "Top-secret data 🙈";
+    }
+}
